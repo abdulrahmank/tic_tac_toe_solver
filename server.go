19aa25a2acdef31f5c14f809f2e26ebf1ab15a24
Abdul Rahman <kadkab.abdul@gmail.com>
2019-04-16 01:21:20 +0530

    Listen to port set in env var

--- server.go
+++ server.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"github.com/abdulrahmank/solver/tic_tac_toe/handler"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "80"
+	}
 	http.HandleFunc("/play", handler.Play)
-	log.Panic(http.ListenAndServe(":80", nil))
+	log.Panic(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
